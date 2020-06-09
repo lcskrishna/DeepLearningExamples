291672964d683b619e64161492568500b986af7b
lcskrishna <lollachaitanya@gmail.com>
2020-06-08 22:54:51 -0700

    update nv files

--- PyTorch/LanguageModeling/BERT/benchmark-scripts/nv/run_all.sh
+++ PyTorch/LanguageModeling/BERT/benchmark-scripts/nv/run_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bash benchmark-scripts/run_benchmarks.sh
-bash benchmark-scripts/get_final_results.sh
+bash benchmark-scripts/nv/run_benchmarks.sh
+bash benchmark-scripts/nv/get_final_results.sh
